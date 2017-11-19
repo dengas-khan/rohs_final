942b9e88c8b8c4274085e01f50c3f4bb9172915e
dengas-khan <32390675+dengas-khan@users.noreply.github.com>
2017-11-19 16:23:04 +0000

    Update player.py

--- rohs/player.py
+++ rohs/player.py
@@ -1,24 +1,22 @@
 class Hero(object):
-    def __init__(self, name):
+    def __init__(self, name, deck):
         self.name = name
         self.health = 30
         def get_devotion1():
             x = input('Enter your primary devotion ' + self.name + ': ')
             elements = ['earth', 'fire', 'water', 'wind', 'lightning']
-            try:
-                x in elements
+            if x in elements:
                 return x
-            except:
+            else:
                 return get_devotion1()
         def get_devotion2():
             y = input('Enter your second devotion ' + self.name + ': ')
             elements = ['earth', 'fire', 'water', 'wind', 'lightning']
-            try:
-                if y in elements:
-                    return y
-                elif y == 'none':
-                    return None
-            except:
+            if y in elements:
+                return y
+            elif y == 'none':
+                return None
+            else:
                 return get_devotion2()
             
         self.devotion1 = get_devotion1()
@@ -28,9 +26,12 @@ class Hero(object):
         self.fire = 0
         self.water = 0
         self.lightning = 0
+        self.field = list()
+        self.hand = list()
+        self.deck = deck
 
-Player1 = Hero('Player1')
-Player2 = Hero('Player2')
+Player1 = Hero('Player1', [])
+Player2 = Hero('Player2', [])
 
 def test():
     print(Player1.name)
